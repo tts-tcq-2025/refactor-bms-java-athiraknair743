1eddd56ef695680c9438860d78fff3544c784d19
athiraknair743 <athirakookkal@gmail.com>
2025-08-04 21:19:59 +0530

    Update VitalsChecker.java

--- src/main/java/vitals/VitalsChecker.java
+++ src/main/java/vitals/VitalsChecker.java
@@ -2,31 +2,44 @@ package vitals;
 
 
 public abstract class VitalsChecker {
-  static boolean vitalsOk(float temperature, float pulseRate, float spo2) 
-      throws InterruptedException {
-    if (temperature > 102 || temperature < 95) {
-      return handleError("Temperature is critical!");
-    } else if (pulseRate < 60 || pulseRate > 100) {
-      return handleError("Pulse Rate is out of range!");
-    } else if (spo2 < 90) {
-      return handleError("Oxygen Saturation out of range!");
+    static boolean vitalsOk(float temperature, float pulseRate, float spo2) throws InterruptedException {
+        if (!isTemperatureNormal(temperature)) {
+            return handleError("Temperature is critical!");
+        }
+        if (!isPulseRateNormal(pulseRate)) {
+            return handleError("Pulse Rate is out of range!");
+        }
+        if (!isSpo2Normal(spo2)) {
+            return handleError("Oxygen Saturation out of range!");
+        }
+        return true;
     }
-    return true;
-  }
 
-  private static void showAlertAnimation() throws InterruptedException {
-     for (int i = 0; i < 6; i++) {
-        System.out.print("\r* ");
-        Thread.sleep(1000);
-        System.out.print("\r *");
-        Thread.sleep(1000);
-      }
-  } 
+    private static boolean isTemperatureNormal(float temperature) {
+        return temperature >= 95 && temperature <= 102;
+    }
 
- private static boolean handleError(String errorMessage) throws InterruptedException{
-    System.out.println(errorMessage);
-    showAlertAnimation();
-    return false;
- }
-  
+    private static boolean isPulseRateNormal(float pulseRate) {
+        return pulseRate >= 60 && pulseRate <= 100;
+    }
+
+    private static boolean isSpo2Normal(float spo2) {
+        return spo2 >= 90;
+    }
+
+    private static void showAlertAnimation() throws InterruptedException {
+        for (int i = 0; i < 6; i++) {
+            System.out.print("\r* ");
+            Thread.sleep(1000);
+            System.out.print("\r *");
+            Thread.sleep(1000);
+        }
+    }
+
+    private static boolean handleError(String errorMessage) throws InterruptedException {
+        System.out.println(errorMessage);
+        showAlertAnimation();
+        return false;
+    }
+
 }
